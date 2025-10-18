366efa1abfc7713421c5f432fd8f50a3cc87378a
khizar <masthantech619@gmail.com>
2025-10-18 22:37:24 +0530

    modifying roboshop script

--- frontend.sh
+++ frontend.sh
@@ -1,38 +1,4 @@
-Start_time=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-g="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-LOG_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-mkdir -p $LOG_FOLDER
-
-echo  "Script started running at: $(date)" | tee -a $LOG_FILE
-
-CHECK_ROOT () {
-    if [ $USERID -ne 0 ] 
-    then 
-        echo -e " $R ERROR...Please run this script with root access $N" | tee -a $LOG_FILE
-        exit 1
-    else 
-        echo -e " $Y You are running the script with root access $N" | tee -a $LOG_FILE   
-    fi     
-}
-
-VALIDATE () {
-    if [ $1 -ne 0 ]
-    then 
-        echo -e  "$2 is.... $R ERROR $N" | tee -a $LOG_FILE
-        exit 1
-    else 
-        echo -e   "$2 is....$G SUCCESS $N" | tee -a $LOG_FILE  
-    fi    
-}
-
+source ./common
 
 CHECK_ROOT 
 
@@ -70,9 +36,4 @@ VALIDATE $? "Copying the nginx conf file "
 systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarting the nginx"
 
-End_time=$(date +%s)
-
-Total_time=$(( $End_time - $Start_time ))
-
-echo -e "Script executed successfully, $Y Time taken : $Total_time Seconds $N" | tee -a $LOG_FILE
-
+PRINT_TIME
